3c85b700b7983a6fc936ac7c87f4324a46b7c612
Kayle Sillito <kaylebug1@yahoo.com>
2016-09-28 16:23:18 -0600

    test

---
+++ web/forms.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Confirmation Page</title>
+            </head>
+	<body >
+		Name: <?php echo $_POST["name"]; ?><br>
+		Email: <?php echo $_POST["email"]; ?><br>
+		Major: <?php echo $_POST["major"]; ?><br>
+		Comments: <?php echo $_POST["comment"]; ?><br>
+	</body>
+</html>
